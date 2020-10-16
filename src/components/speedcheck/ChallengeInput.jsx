e76c3a452abc88dcb1325f8a0b9077eae306d7b7
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-10-16 15:25:52 +0300

    text-control 2

--- src/components/speedcheck/ChallengeInput.jsx
+++ src/components/speedcheck/ChallengeInput.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const initialState = {
     entry: '',
-    isDisabled: false
+    isDisabled: false,
+    start:null,
+    end:null
 }
 
 class ChallengeInput extends React.Component {
@@ -12,9 +14,14 @@ class ChallengeInput extends React.Component {
     keyMap = []
 
     changeHandler = (e) => {
+        let { start } = this.state;
+        if (start === null) {
+            start = new Date().getTime();
+        }
         this.setState({
             ...this.state,
-            [e.target.name] : e.target.value
+            [e.target.name] : e.target.value,
+            start
         })
     }
 
@@ -27,13 +34,15 @@ class ChallengeInput extends React.Component {
         if (this.keyMap[17] && this.keyMap[13]) {
             this.setState({
                 ...this.state,
-                isDisabled: true
+                isDisabled: true,
+                end: new Date().getTime()
             })
+            this.checkEntyr()
         }
     }
 
     checkEntyr = () => {
-        const { entry } = this.state;
+        const { entry, end, start } = this.state;
         let sum = 0;
         const arr_challenge = this.challenge.split(' ');
         const arr_entry = entry.split(' ');
@@ -46,8 +55,16 @@ class ChallengeInput extends React.Component {
         });
         sum = sum + arr_entry.length -1;
         let accuracy = (sum * 100 / this.challenge.length);
-        let duration = 0;
+        let duration = (end - start) / 1000;
         let wordsPerMinute = (entry.length * 60 ) / (6 * duration);
+
+        const result = {
+            duration,
+            accuracy,
+            wordsPerMinute
+        }
+
+        this.props.setResult(result)
     }
 
 
