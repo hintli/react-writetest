e0a5acc40aa5612033b3258b75ad5183dfb60f60
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-10-16 12:22:11 +0300

    speedcheck component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
             <NavLink className="navbar-brand" to="/">Yaz Test</NavLink>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
--- src/components/speedcheck/SpeedCheck.jsx
+++ src/components/speedcheck/SpeedCheck.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-function SpeedCheck() {
-    return (
-        <div>
-            AnaSayfa
-        </div>
-    )
+class SpeedCheck extends Component {
+    state = {}
+    render() {
+        return (
+            <React.Fragment>
+                <div className="card text-center border border-info">
+                    <div className="card-header">
+                        <h3>Yazma Hızı Testi</h3>
+                    </div>
+                    <div className="card-body">
+                        <p>Alttaki metni kutucuğa girmeye başladığınızda süreniz başlayacaktır.</p>
+                        <p>Testi bitirmek için 'ctrl + Enter' kombinasyonunu kullanınız.</p>
+                        <p>Ayrıca karakter sınırını aştığınızda test otomatik olarak sonlanacaktır.</p>
+                    </div>
+                    <div className="card-footer">
+                        Hello React
+                </div>
+                </div>
+                <hr />
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Metni giriniz" />
+                    <div className="input-group-append">
+                        <button className="btn btn-outline-secondary" type="button" id="reset">sıfırla</button>
+                    </div>
+                </div>
+                <hr />
+                <div className="card-deck">
+                    <div className="card">
+                        <div className="card-body">
+                            Süre:
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body">
+                            Doğruluk:
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body">
+                            DBK:
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
+
 
 export default SpeedCheck;
