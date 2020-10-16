e6d6fb95cb45b2f35072fc3667b5228afb1f7a3a
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-10-16 15:04:18 +0300

    text-control 1

--- src/components/speedcheck/ChallengeInput.jsx
+++ src/components/speedcheck/ChallengeInput.jsx
@@ -31,6 +31,26 @@ class ChallengeInput extends React.Component {
             })
         }
     }
+
+    checkEntyr = () => {
+        const { entry } = this.state;
+        let sum = 0;
+        const arr_challenge = this.challenge.split(' ');
+        const arr_entry = entry.split(' ');
+        arr_challenge.forEach((c, i) => {
+            for (let j = 0; j < c.length; j++) {
+                if (arr_entry[i] && c[j] === arr_entry[i][j]) {
+                    sum = sum + 1;
+                }
+            }
+        });
+        sum = sum + arr_entry.length -1;
+        let accuracy = (sum * 100 / this.challenge.length);
+        let duration = 0;
+        let wordsPerMinute = (entry.length * 60 ) / (6 * duration);
+    }
+
+
     keyUphandler = () => {
         this.keyMap = []
     }
@@ -39,6 +59,11 @@ class ChallengeInput extends React.Component {
     componentDidMount() {
         document.addEventListener("keydown", this.keyDownHandler);
         document.addEventListener("keyup", this.keyUphandler);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("keydown", this.keyDownHandler);
+        document.removeEventListener("keyup", this.keyUphandler);
     }
 
     render() {
